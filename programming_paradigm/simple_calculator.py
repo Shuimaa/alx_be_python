d67e8d17ae2e53025d51ce631c269eda970f3724
Shaimaa Walid <alshymawlyd01@gmail.com>
2025-08-04 00:15:26 +0300

    Fix test import and add simple_calculator module

---
+++ programming_paradigm/simple_calculator.py
@@ -0,0 +1,28 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+        self.assertEqual(self.calc.subtract(0, 7), -7)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        with self.assertRaises(ValueError):
+            self.calc.divide(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
